07deb064ad67978d80990da394f9fa9b7f88f86c
moh <osman.elrashid@gmail.com>
2019-07-28 17:10:42 +0400

    initial files

---
+++ android/app/src/main/java/com/example/e009_flutter_dart_02_03_00_collection_for_e004base/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.e009_flutter_dart_02_03_00_collection_for_e004base;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
